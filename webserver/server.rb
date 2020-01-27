582cac6fa57ea681f9bd905be1b5ae01d704e4bf
Oliver Tobias Kreuzer <oliver.kreuzer@gmx.at>
2020-01-27 15:51:18 +0000

    Webserver almost done

--- webserver/server.rb
+++ webserver/server.rb
@@ -1,24 +1,68 @@
 require 'sinatra/base'
 require 'sinatra-websocket'
-
-#set :server, 'thin'
-#set :sockets, []
+require 'json'
 
 Choices = {
   'ON' => 'Turn LED ON',
   'OFF' => 'Turn LED OFF',
 }
 
+def is_json?(json)
+  begin
+    JSON.parse(json)
+    return true
+  rescue Exception => e
+    return false
+  end
+end
+
 class MyApp < Sinatra::Base
+  set :server, 'thin'
+  set :sockets, []
+
   set :bin, '0.0.0.0'
   set :environment, :production
 
-  get "/" do
-    @title = "Welcome to the Sato Lab IoT Project Web Interface!"
-    erb :index
-  end
+  @@gateway_connected = 0 #class variable
+  @msg_content = ""
 
-  post "/actuate" do
+  get '/' do
+    if !request.websocket?
+      @title = 'Welcome to the Sato Lab IoT Project Web Interface!'
+      if @@gateway_connected == 0
+        erb :index
+      else #gateway is connected -> serve the Control Interface
+        erb :connected
+      end
+    else
+      @@gateway_connected = 1
+      request.websocket do |ws|
+        ws.onopen do
+          ws.send("Hello World!")
+          settings.sockets << ws           
+        end
+        ws.onmessage do |msg|
+          #EM.next_tick { settings.sockets.each{|s| s.send(msg) } }
+          # check if the message has a known format and pass it to javascript
+          if is_json?(msg) == true
+            msg_json = JSON.parse(msg)
+            if (msg_json.key?("temp") && msg_json.key?("humid") && msg_json.key?("time"))
+              @msg_content = msg_json
+            end
+          else
+            puts "No valid JSON"
+          end
+        end
+        ws.onclose do
+          @@gateway_connected = 0
+          warn("websocket closed")
+          settings.sockets.delete(ws)
+        end
+      end
+    end
+  end
+  
+  post '/actuate' do
     erb :index
   end
 
