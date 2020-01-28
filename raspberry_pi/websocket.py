c3d82c3e24a21eb731cc43ac6203d7c17b8ffa74
Oliver Tobias Kreuzer <oliver.kreuzer@gmx.at>
2020-01-28 13:50:32 +0900

    websocket file init

---
+++ raspberry_pi/websocket.py
@@ -0,0 +1,18 @@
+import asyncio
+import websockets
+
+async def wsRun():
+  async with websockets.connect('ws://79cb755.online-server.cloud:4567') as websocket:
+    #await websocket.send("hello from pi")
+    #print("From the server:")
+
+    while True:
+      response = await websocket.recv()
+      #print(response)
+      if response == "LED = ON":
+        
+
+def wsStart():
+  loop = asyncio.new_event_loop()
+  loop.run_until_complete(wsRun())
+
