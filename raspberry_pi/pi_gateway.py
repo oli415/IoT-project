dc0b25615659e5e04939ec79fa99eeec7285961c
Oliver Tobias Kreuzer <oliver.kreuzer@gmx.at>
2020-01-29 11:29:27 +0900

    tried without class -> same problem

--- raspberry_pi/pi_gateway.py
+++ raspberry_pi/pi_gateway.py
@@ -3,23 +3,23 @@ import sys
 import arduino_comm
 import websocket
 
+arduino_comm.init()
 # start thread for receiving from Arduino
-comm1 = arduino_comm.ArduinoComm()
 try:
-  receiveThread = threading.Thread(target = comm1.waitForNotifications)
+  receiveThread = threading.Thread(target = arduino_comm.start)
   receiveThread.daemon = True # the entire python program exits when only daemon threads are left
   receiveThread.start()
 except:
   print("Error: unable to start Arduino receive thread")
 
 # start thread for communicating with webserver
-ws1 = websocket.WebSocket()
-try:
-  socketThread = threading.Thread(target = ws1.start)
-  socketThread.daemon = True
-  socketThread.start()
-except:
-  print("Error: unable to start websocket comm thread")
+#ws1 = websocket.WebSocket()
+#try:
+ # socketThread = threading.Thread(target = ws1.start)
+  #socketThread.daemon = True
+  #socketThread.start()
+#except:
+ # print("Error: unable to start websocket comm thread")
 
 # start program (user input) loop
 while 1:
@@ -27,12 +27,12 @@ while 1:
   userInput = input()
   #print "You entered %s" % userInput
   if userInput == "exit":
-    comm1.arduinoDisconnect()
+    arduino_comm.arduinoDisconnect()
     break
   elif userInput == "led on":
-    comm1.sendToArduino(b"#LED = ON#")
+    arduino_comm.sendToArduino(b"#LED = ON#")
   elif userInput == "led off":
-    comm1.sendToArduino(b"#LED = OFF#")
+    arduino_comm.sendToArduino(b"#LED = OFF#")
   else:
     print("Command unknown!")
 
