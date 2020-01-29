dfda2dffb1eed76147ef339ff3796f2bedee157c
Oliver Tobias Kreuzer <oliver.kreuzer@gmx.at>
2020-01-29 11:23:24 +0000

    displaying sensor data working

--- webserver/server.rb
+++ webserver/server.rb
@@ -25,7 +25,7 @@ class MyApp < Sinatra::Base
   set :environment, :production
 
   @@gateway_connected = 0 #class variable
-  @msg_content = "" #instance variable
+  @@msg_content = ""
 
   get '/' do
     if !request.websocket?
@@ -48,7 +48,7 @@ class MyApp < Sinatra::Base
           if is_json?(msg) == true
             msg_json = JSON.parse(msg)
             if (msg_json.key?("T") && msg_json.key?("H") && msg_json.key?("t"))
-              @msg_content = msg
+              @@msg_content = msg_json
             end
           else
             puts "No valid JSON"
@@ -70,6 +70,17 @@ class MyApp < Sinatra::Base
       s.send("LED = " + @act) } #send command to turn on or off LED
     erb :connected
   end
+
+  post '/display' do
+    @title = 'Welcome to the Sato Lab IoT Project Web Interface!'
+    if @@msg_content == ""
+      @data = "No data available"
+    else
+      @data = "T = " + @@msg_content["T"] + ", H = " + @@msg_content["H"] + ", t = " + @@msg_content["t"]
+    end
+    erb :connected
+  end
+
 
   run! if app_file == $0
 end
